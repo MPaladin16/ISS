a46040fc393f95079e9d95ced09f94380454f3d8
MPaladin16 <mp52512@fer.hr>
2021-11-06 09:44:39 +0100

    Dodani ljudi i hodanje
    Treba debuggirat zasto spawner ide naprijed

---
+++ ISS - Car Parking/Assets/Distant Lands/Free Characters/Contents/Prefabs/Walking.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Walking : MonoBehaviour
+{
+    void OnAnimatorMove()
+    {
+        Animator animator = GetComponent<Animator>();
+
+        if (animator)
+        {
+            Vector3 newPosition = transform.position;
+            newPosition.z += 5* Time.deltaTime;
+            transform.position = newPosition;
+        }
+    }
+}
